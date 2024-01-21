23759003de84d6047548672a979f06f48e34fd41
Arthur Bacci <arthurbacci@protonmail.com>
2024-01-20 23:32:01 -0300

    chore: better dev.sh

--- dev.sh
+++ dev.sh
@@ -2,3 +2,8 @@
 
 CFLAGS='-Wall -Wextra -pedantic -Wshadow -Werror -Wstrict-overflow -fsanitize=address -fsanitize=undefined' make
 
+echo 'COMPILED! PRESS ENTER TO CONTINUE'
+read something
+
+./cupidfetch 2> log.txt
+
