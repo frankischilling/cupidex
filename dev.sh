ae019c48e40348b9a521a6413c02b375f210a89d
Frank <101807917+frankischilling@users.noreply.github.com>
2024-11-03 19:04:15 -0500

    Update dev.sh

--- dev.sh
+++ dev.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
 
-CFLAGS='-Wall -Wextra -pedantic -Wshadow -Werror -Wstrict-overflow -fsanitize=address -fsanitize=undefined' make
+CFLAGS='-Wall -Wextra -pedantic -Wshadow -Werror -Wstrict-overflow -fsanitize=address -fsanitize=undefined'
+LDFLAGS='-lmagic -lcurses'
+cc -o cupidfm src/*.c --std=c2x $CFLAGS $LDFLAGS -Isrc
 
 echo 'COMPILED! PRESS ENTER TO CONTINUE'
 read something
 
 ./cupidfm 2> log.txt
-
