d2c8d306bc64188c979bcf2453ae18333e18668f
ferruccia1atwit <156090775+ferruccia1atwit@users.noreply.github.com>
2025-01-28 14:26:33 -0500

    fix on ubuntu wsl, fix default config typ

--- src/ui.h
+++ src/ui.h
@@ -1,2 +1,10 @@
+// ui.h
+#ifndef UI_H
+#define UI_H
+
+#include <ncurses.h>
+
 void show_notification(WINDOW *win, const char *format, ...);
 void show_popup(const char *title, const char *fmt, ...);
+
+#endif // UI_H
