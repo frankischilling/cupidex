5557ee2309970cf1a1121f9ed61b8ad83acc5061
Frank <101807917+frankischilling@users.noreply.github.com>
2025-02-05 12:39:08 -0500

    revert until fix ghost char issue when using (maybe wsl)

--- dev.sh
+++ dev.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 CFLAGS='-Wall -Wextra -pedantic -Wshadow -Werror -Wstrict-overflow -fsanitize=address -fsanitize=undefined'
-LDFLAGS='-lmagic -lncursesw'
+LDFLAGS='-lmagic -lcurses'
 cc -o cupidfm src/*.c --std=c2x $CFLAGS $LDFLAGS -Isrc
 
 echo 'COMPILED! PRESS ENTER TO CONTINUE'
